650da75a06f6bc16a78dc4673e9d9e1f36b77ca2
unknown <smititaliya40@gmail.com>
2023-08-16 14:44:23 +0530

    y.cpp added

---
+++ y.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+    
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+         int n; 
+         cin>>n;
+        cout<<"hello"; 
+    
+    }
+    
+    return 0;
+}
